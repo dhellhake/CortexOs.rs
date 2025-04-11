4d43b2d3c7d773a786856f42f84eb9918cbc5902
dhellhake <dominik_hellhake@msn.com>
2025-04-11 21:44:28 +0200

    Code-Cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,87 +9,11 @@ pub mod os;
 pub mod cortex;
 pub mod peripherals;
 
-use core::{arch::asm, mem, ops::DerefMut, panic::PanicInfo};
+use core::{arch::asm, ops::DerefMut, panic::PanicInfo};
 
 use cortex::{scb::{SystemControlBlock, SCB}, systick::{SysTick, SystemTimer}};
 use os::{OperatingSystem, Os, Task};
 use peripherals::port::{IOPinController, PORT};
-
-#[no_mangle]
-pub unsafe extern "C" fn SysTick_Isr() {  
-    cortex::CriticalSection(|st| {
-        if let Some(ref mut scb) = SCB.borrow(st).borrow_mut().deref_mut() {
-            scb.Set_PendSV();
-        }
-    });
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn PendSV() {
-    os::OsSection(|st| {
-        if let Some(ref mut os) = Os.borrow(st).borrow_mut().deref_mut() {
-            let [ref mut a, ref mut b, ..] = os.tasks;
-            if os.taskIdx == 0 {
-                ContextSwitch(a, b);
-
-                let stackIdx = 255 - ((((&a.stack[255]) as *const u32) as u32) - a.sp) / 4;
-
-                a.stack[stackIdx as usize + 14] = (cyclic as *const ()) as u32;
-                for stackOffset in 1..6 {
-                    a.stack[stackIdx as usize + 14 - stackOffset as usize] = 0;
-                }
-                a.stack[stackIdx as usize + 14 - 6] = (a as *const Task) as u32;
-
-                os.taskIdx = 1;
-            } else {
-                ContextSwitch(b, a);
-
-                let stackIdx = 255 - ((((&b.stack[255]) as *const u32) as u32) - b.sp) / 4;
-
-                b.stack[stackIdx as usize + 14] = (cyclic as *const ()) as u32;
-                for stackOffset in 1..6 {
-                    b.stack[stackIdx as usize + 14 - stackOffset as usize] = 0;
-                }
-                b.stack[stackIdx as usize + 14 - 6] = (b as *const Task) as u32;
-
-                os.taskIdx = 0;
-            }
-        }
-    });
-}
-
-pub fn ContextSwitch(task0: &mut Task, task1: &mut Task)
-{
-    let mut t0sp: u32 = ((&task0.sp) as *const u32) as u32;
-    let mut t1sp: u32 = ((&task1.sp) as *const u32) as u32;
-    unsafe {
-        asm!("cpsid i");
-        asm!("mrs	r0, psp");
-        asm!("subs	r0, #16");
-        asm!("stmia	r0!,{{r4-r7}}");
-        asm!("mov	r4, r8");
-        asm!("mov	r5, r9");
-        asm!("mov	r6, r10");
-        asm!("mov	r7, r11");
-        asm!("subs	r0, #32");
-        asm!("stmia	r0!,{{r4-r7}}");
-        asm!("subs	r0, #16");
-
-        asm!("str	r0, [r1]", inout("r1") t0sp);
-
-        asm!("ldr	r0, [r1]", inout("r1") t1sp);
-        
-        asm!("ldmia	r0!,{{r4-r7}}");
-        asm!("mov	r8, r4");
-        asm!("mov	r9, r5");
-        asm!("mov	r10, r6");
-        asm!("mov	r11, r7");
-        asm!("ldmia	r0!,{{r4-r7}}");
-        asm!("msr	psp, r0");
-        asm!("ldr r0, =0xFFFFFFFD");
-        asm!("cpsie	i");
-    }
-}
 
 
 fn taskone(_tstmp: u32) {
@@ -107,14 +31,6 @@ fn tasktwo(_tstmp: u32) {
     });
 }
 
-//tskref: *const Task
-fn cyclic(task: *const Task) {
-    unsafe {
-        ((*task).cyclic)(123);
-    }
-    loop { }
-}
-
 fn main() -> ! {
     cortex::CriticalSection(|st| {
         SysTick.borrow(st).replace(Some(SystemTimer::new().unwrap()));
@@ -127,7 +43,6 @@ fn main() -> ! {
             port.Set_PinDirection(1, 9, true);
         }
     });
-
     
     os::OsSection(|ot| {
         Os.borrow(ot).replace(Some(OperatingSystem::new().unwrap()));
@@ -136,17 +51,9 @@ fn main() -> ! {
     let mut stack: u32 = 0;
     os::OsSection(|st| {
         if let Some(ref mut os) = Os.borrow(st).borrow_mut().deref_mut() {
-            os.tasks[0].cyclic = taskone;
-            os.tasks[0].sp = ((&os.tasks[0].stack[256 - 16]) as *const u32) as u32;
-            os.tasks[0].stack[256 - 1] = 0x01000000;
-            os.tasks[0].stack[256 - 2] = (cyclic as *const ()) as u32;
-            os.tasks[0].stack[256 - 8] = ((os.tasks.as_ptr() as usize) + (mem::size_of::<Task>() * 0)) as u32;
-
-            os.tasks[1].cyclic = tasktwo;
-            os.tasks[1].sp = ((&os.tasks[1].stack[256 - 16]) as *const u32) as u32;
-            os.tasks[1].stack[256 - 1] = 0x01000000;
-            os.tasks[1].stack[256 - 2] = (cyclic as *const ()) as u32;
-            os.tasks[1].stack[256 - 8] = ((os.tasks.as_ptr() as usize) + (mem::size_of::<Task>() * 1)) as u32;
+            os.SetTask(0, taskone);
+            
+            os.SetTask(1, tasktwo);
 
             stack = (&(os.tasks[0].stack[256 - 16]) as *const u32) as u32;
         }
@@ -179,7 +86,7 @@ fn main() -> ! {
     });
 
 
-    cyclic(startTask);
+    os::cyclic(startTask);
 
     loop {}
 }
--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,4 +1,6 @@
-use core::cell::{RefCell, UnsafeCell};
+use core::{arch::asm, cell::{RefCell, UnsafeCell}, mem, ops::DerefMut};
+
+use crate::{cortex, SCB};
  
 pub(crate) static Os: OsMutex<RefCell<Option<OperatingSystem>>> = OsMutex::new(RefCell::new(None));
 
@@ -14,6 +16,43 @@ pub(crate) static Os: OsMutex<RefCell<Option<OperatingSystem>>> = OsMutex::new(R
  fn empty(_tstmp: u32) {
     loop {}
  }
+
+ pub fn ContextSwitch(task0: &mut Task, task1: &mut Task)
+{
+    let mut t0sp: u32 = ((&task0.sp) as *const u32) as u32;
+    let mut t1sp: u32 = ((&task1.sp) as *const u32) as u32;
+    unsafe {
+        asm!(
+            "cpsid i",
+            "mrs	r0, psp",
+            "subs	r0, #16",
+            "stmia	r0!,{{r4-r7}}",
+            "mov	r4, r8",
+            "mov	r5, r9",
+            "mov	r6, r10",
+            "mov	r7, r11",
+            "subs	r0, #32",
+            "stmia	r0!,{{r4-r7}}",
+            "subs	r0, #16",
+            "str	r0, [r1]",
+            inout("r1") t0sp,
+        );
+
+        asm!(
+            "ldr	r0, [r1]",
+            "ldmia	r0!,{{r4-r7}}",
+            "mov	r8, r4",
+            "mov	r9, r5",
+            "mov	r10, r6",
+            "mov	r11, r7",
+            "ldmia	r0!,{{r4-r7}}",
+            "msr	psp, r0",
+            "ldr r0, =0xFFFFFFFD",
+            "cpsie	i",
+            inout("r1") t1sp,
+        );
+    }
+}
 
  #[derive(Copy, Clone, Debug)]
 pub enum TaskStatus
@@ -50,13 +89,67 @@ impl OperatingSystem {
             None
         }
     }
+
+    #[inline]
+    pub fn SetTask(&mut self, tIdx: usize, func: fn(u32)) {
+        self.tasks[tIdx].cyclic = func;
+        self.tasks[tIdx].sp = ((&self.tasks[tIdx].stack[256 - 16]) as *const u32) as u32;
+        self.tasks[tIdx].stack[256 - 1] = 0x01000000;
+        self.tasks[tIdx].stack[256 - 2] = (cyclic as *const ()) as u32;
+        self.tasks[tIdx].stack[256 - 8] = ((self.tasks.as_ptr() as usize) + (mem::size_of::<Task>() * tIdx)) as u32;
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn SysTick_Isr() {  
+    cortex::CriticalSection(|st| {
+        if let Some(ref mut scb) = SCB.borrow(st).borrow_mut().deref_mut() {
+            scb.Set_PendSV();
+        }
+    });
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn PendSV() {
+    OsSection(|st| {
+        if let Some(ref mut os) = Os.borrow(st).borrow_mut().deref_mut() {
+            let [ref mut a, ref mut b, ..] = os.tasks;
+            if os.taskIdx == 0 {
+                ContextSwitch(a, b);
+
+                let stackIdx = 255 - ((((&a.stack[255]) as *const u32) as u32) - a.sp) / 4;
+
+                a.stack[stackIdx as usize + 14] = (cyclic as *const ()) as u32;
+                for stackOffset in 1..6 {
+                    a.stack[stackIdx as usize + 14 - stackOffset as usize] = 0;
+                }
+                a.stack[stackIdx as usize + 14 - 6] = (a as *const Task) as u32;
+
+                os.taskIdx = 1;
+            } else {
+                ContextSwitch(b, a);
+
+                let stackIdx = 255 - ((((&b.stack[255]) as *const u32) as u32) - b.sp) / 4;
+
+                b.stack[stackIdx as usize + 14] = (cyclic as *const ()) as u32;
+                for stackOffset in 1..6 {
+                    b.stack[stackIdx as usize + 14 - stackOffset as usize] = 0;
+                }
+                b.stack[stackIdx as usize + 14 - 6] = (b as *const Task) as u32;
+
+                os.taskIdx = 0;
+            }
+        }
+    });
 }
 
 
-
-
-
-
+pub fn cyclic(task: *const Task) {
+    unsafe {
+        ((*task).cyclic)(123);
+    }
+    loop { }
+}
 
 
 
